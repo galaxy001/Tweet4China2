701500155f3f8d13856600d1918b2481dc0c0c2e
Jason Hsu <support@tuoxie.me>
2013-09-10 15:23:14 +0800

    add progress bar to webview

--- iOS/Tweet4China/src/HSUDiscoverViewController.m
+++ iOS/Tweet4China/src/HSUDiscoverViewController.m
@@ -7,14 +7,18 @@
 //
 
 #import "HSUDiscoverViewController.h"
+#import "NJKWebViewProgress.h"
 
 #define StartURL @"http://m.facebook.com"
 
-@interface HSUDiscoverViewController () <UITextFieldDelegate, UIWebViewDelegate, UIScrollViewDelegate>
+@interface HSUDiscoverViewController () <UITextFieldDelegate, UIWebViewDelegate, UIScrollViewDelegate, NJKWebViewProgressDelegate>
 
 @property (nonatomic, weak) UIWebView *webView;
 @property (nonatomic, weak) UITextField *urlTextField;
 @property (nonatomic, weak) UIView *tabBarBackground;
+@property (nonatomic, strong) NSURL *currentURL;
+@property (nonatomic, strong) NJKWebViewProgress *progressHandler;
+@property (nonatomic, weak) UIProgressView *progressBar;
 
 @end
 
@@ -22,16 +26,20 @@
 
 - (void)viewDidLoad
 {
+    self.progressHandler = [[NJKWebViewProgress alloc] init];
+    self.progressHandler.webViewProxyDelegate = self;
+    self.progressHandler.progressDelegate = self;
+    
     UIWebView *webView = [[UIWebView alloc] init];
     [self.view addSubview:webView];
     self.webView = webView;
-    webView.delegate = self;
     webView.scrollView.delegate = self;
     webView.scalesPageToFit = YES;
     webView.allowsInlineMediaPlayback = YES;
     webView.backgroundColor = kWhiteColor;
     [webView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:StartURL]]];
     webView.frame = self.view.bounds;
+    webView.delegate = self.progressHandler;
     
     self.hideRightButtons = YES;
     self.useRefreshControl = NO;
@@ -50,9 +58,15 @@
     urlTextField.delegate = self;
     urlTextField.keyboardType = UIKeyboardTypeURL;
     urlTextField.clearButtonMode = UITextFieldViewModeWhileEditing;
-    urlTextField.placeholder = @"Type URL";
+    urlTextField.placeholder = @"Enter URL";
     urlTextField.backgroundColor = bw(245);
     urlTextField.layer.sublayerTransform = CATransform3DMakeTranslation(5, 0, 0);
+    
+    UIProgressView *progressBar = [[UIProgressView alloc] init];
+    [self.navigationController.navigationBar addSubview:progressBar];
+    progressBar.top = self.navigationController.navigationBar.height - progressBar.height;
+    progressBar.width = self.navigationController.navigationBar.width;
+    self.progressBar = progressBar;
     
     [super viewWillAppear:animated];
 }
@@ -99,6 +113,15 @@
     return NO;
 }
 
+- (BOOL)webView:(UIWebView *)webView shouldStartLoadWithRequest:(NSURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType
+{
+    if ([request.URL.absoluteString hasPrefix:@"webviewprogressproxy"]) {
+        return YES;
+    }
+    self.currentURL = request.URL;
+    return YES;
+}
+
 - (void)webViewDidFinishLoad:(UIWebView *)webView
 {
     NSString *urlString = webView.request.URL.absoluteString;
@@ -108,11 +131,29 @@
     self.urlTextField.text = urlString;
 }
 
+- (void)webViewProgress:(NJKWebViewProgress *)webViewProgress updateProgress:(float)progress
+{
+    if (progress == 0.0) {
+        [UIApplication sharedApplication].networkActivityIndicatorVisible = YES;
+        self.progressBar.progress = 0;
+        [UIView animateWithDuration:0.27 animations:^{
+            self.progressBar.alpha = 1.0;
+        }];
+    } else if (progress == 1.0) {
+        [UIApplication sharedApplication].networkActivityIndicatorVisible = NO;
+        [UIView animateWithDuration:0.27 delay:progress - self.progressBar.progress options:0 animations:^{
+            self.progressBar.alpha = 0.0;
+        } completion:nil];
+    }
+    
+    [self.progressBar setProgress:progress animated:NO];
+}
+
 - (void)scrollViewDidScroll:(UIScrollView *)scrollView
 {
     [self.urlTextField resignFirstResponder];
-    if (!self.urlTextField.isEditing && self.webView.request.URL.absoluteString.length) {
-        self.urlTextField.text = self.webView.request.URL.absoluteString;
+    if (!self.urlTextField.isEditing && self.currentURL) {
+        self.urlTextField.text = self.currentURL.absoluteString;
     }
 }
 
